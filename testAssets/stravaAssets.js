fbcbc466588bceffd82341bea4be3489a5d02240
Bri Davis <bridavis@Bris-iMac.local>
2020-08-21 20:47:09 -0500

    updated time for next test

--- testAssets/stravaAssets.js
+++ testAssets/stravaAssets.js
@@ -3,7 +3,7 @@ module.exports = [
     {
         distance: '2',
         minutes: '30',
-        time: '4:00 AM',
+        time: '6:10 AM', //must change the time after each test, otherwise the page will crash (bug!).
         title: 'Test Manual Activity',
         description: 'Just testing this feature. Will delete.'
     },
